8f7438570ccbab88c8e490f641e059bf30e65c92
Damon Collins <damon.c.p.collins@gmail.com>
2019-07-16 13:53:00 -0400

    Subarray Sum Solution

--- algo-practice/Subarray-Sum-Equals-K.js
+++ algo-practice/Subarray-Sum-Equals-K.js
@@ -10,3 +10,19 @@ Note:
 1. The length of the array is in range [1, 20,000].
 2. The range of numbers in the array is [-1000, 1000] and the range of the integer k is [-1e7, 1e7].
 */
+
+function subarraySum (array, num){
+  let count = 0
+  for(let i = 0; i < array.length; i++){
+    let sum = 0
+    for(let j = i; j < array.length; j++){
+      sum += array[j]
+      if(sum === num){
+        count++
+      }
+    }
+  }
+  console.log(count)
+}
+
+subarraySum([1,2,3], 3)
